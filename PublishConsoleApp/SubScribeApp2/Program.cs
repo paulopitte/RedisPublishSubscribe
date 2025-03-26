8a94f010932d99d8af68bd82a343701c820756ea
Paulo Pitte <pauloanalista@outlook.com>
2025-03-26 11:00:49 -0300

    upgrade

---
+++ PublishConsoleApp/SubScribeApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using StackExchange.Redis;
+
+namespace RedisSubscriber;
+class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = ConnectionMultiplexer.Connect("127.0.0.1:6379");
+            var subscriber = connection.GetSubscriber();
+
+            subscriber.Subscribe("order", (channel, message) =>
+            {
+                Console.WriteLine($"{DateTime.UtcNow:o} => {message}");
+            }, CommandFlags.None);
+
+            Console.ReadLine();
+        }
+    }
