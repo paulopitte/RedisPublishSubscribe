d21e80b4378fed6f0db63537fc65daf7fdbbc625
Paulo Pitte <pauloanalista@outlook.com>
2025-03-26 11:15:26 -0300

    implementação basica de conceito publish => subscribe

--- PublishConsoleApp/PublishApp1/Program.cs
+++ PublishConsoleApp/PublishApp1/Program.cs
@@ -1,18 +1,32 @@
 ﻿using StackExchange.Redis;
+using System;
+using System.Threading;
 
-namespace RedisSubscriber;
-class Program
+namespace RedisPublisher
+{
+    class Program
     {
         static void Main(string[] args)
         {
             var connection = ConnectionMultiplexer.Connect("127.0.0.1:6379");
-            var subscriber = connection.GetSubscriber();
 
-            subscriber.Subscribe("order", (channel, message) =>
+            var publisher = connection.GetSubscriber();
+
+            int orderNum = 1;
+            while (true)
             {
-                Console.WriteLine($"{DateTime.UtcNow:o} => {message}");
-            }, CommandFlags.None);
 
-            Console.ReadLine();
+                var random = new Random();
+                var price = Math.Round((decimal)(random.NextDouble() * (1000 - 1) + 1), 2);
+                publisher.Publish("order", $"{{ order: {orderNum}, price: {price} }}");
+
+                Console.ForegroundColor = ConsoleColor.Green; 
+                Console.WriteLine("{ order: " + orderNum + ", price: " + price + "}");
+                Console.ResetColor(); 
+
+                orderNum++;
+                Thread.Sleep(1000);
+            }
         }
     }
+}
